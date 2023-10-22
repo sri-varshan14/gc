72ef2dbb16aadf51eb0bfc154d07bf1f8877de66
sri-varshan14 <mailmeatsri14@gmail.com>
2023-10-23 04:12:04 +0530

    just wrote infinte loop to find the memory is present in stack or not

--- example/main.c
+++ example/main.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 
 int main() {
-  int n = 100000;
+  int n = 10;
   void *arr[n];
   for (int i = 0; i < n; ++i) {
-    arr[i] = xmalloc(4096 * (i + 1));
+    arr[i] = xmalloc(sizeof(int) * n);
+    int *a = arr[i];
+    a[0] = 69;
   }
-  for (int i = n - 1; i >= 0; --i) {
-    xfree(arr[i]);
-  }
+  xheap_collect();
   return 0;
 }
--- include/gc/llallocator.h
+++ include/gc/llallocator.h
@@ -1,26 +1,30 @@
 #ifndef _GC_LLALLOCATOR_H_
 #define _GC_LLALLOCATOR_H_
 
+#include "gc/gc.h"
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
+#include <threads.h>
 #include <unistd.h>
 
 typedef struct xheap_node {
   size_t size;
-  void *mem_ptr;
+  uintptr_t *mem_ptr;
   struct xheap_node *next;
+  bool is_used;
 } xheap_node;
 
 static xheap_node *used_root = NULL;
 static xheap_node *unused_root = NULL;
 static int cxheap_free_node = 0;
 
-void *xmalloc(size_t size);
-void xfree(void *ptr);
+uintptr_t *xmalloc(size_t size);
+void xfree(uintptr_t *ptr);
+void xheap_collect();
 
 xheap_node *__alloc_xheap_node(size_t size, xheap_node *next);
 void __internal_print();
@@ -39,6 +43,7 @@ xheap_node *__alloc_xheap_node(size_t size, xheap_node *next) {
   if (node_mem != MAP_FAILED) {
     node_mem->size = size;
     node_mem->next = next;
+    node_mem->is_used = true;
     node_mem->mem_ptr = mmap(NULL, size, PROT_READ | PROT_WRITE,
                              MAP_ANONYMOUS | MAP_SHARED, -1, 0);
     if (node_mem->mem_ptr == NULL)
@@ -47,8 +52,8 @@ xheap_node *__alloc_xheap_node(size_t size, xheap_node *next) {
   return node_mem;
 }
 
-void *xmalloc(size_t size) {
-  void *return_ptr = NULL;
+uintptr_t *xmalloc(size_t size) {
+  uintptr_t *return_ptr = NULL;
   if (size == 0)
     return NULL;
   xheap_node *mem = __alloc_xheap_node(size, used_root);
@@ -57,7 +62,7 @@ void *xmalloc(size_t size) {
   return used_root->mem_ptr;
 }
 
-void xfree(void *ptr) {
+void xfree(uintptr_t *ptr) {
   xheap_node *cur_n = used_root;
   xheap_node *prev_n = NULL;
   if (ptr == NULL || cur_n == NULL)
@@ -79,6 +84,33 @@ void xfree(void *ptr) {
   cur_n->next = unused_root;
   unused_root = cur_n;
   cxheap_free_node++;
+}
+
+void xheap_collect() {
+  uintptr_t *start = __builtin_frame_address(0);
+  uintptr_t *end = 0;
+  end = end + __alignof__(uintptr_t *);
+  xheap_node *cur = used_root;
+  printf("%p\n", __builtin_frame_address(0));
+  printf("%p\n", __builtin_frame_address(1));
+  printf("%p\n", __builtin_frame_address(2));
+  int count = 1000;
+  while (start > end) {
+    uintptr_t *p = (uintptr_t *)*start;
+    if (used_root->mem_ptr == p) {
+      printf("USED ROOT PTR : %p\n", used_root);
+      printf("USED ROOT MEM PTR ADDR : %p\n", &used_root->mem_ptr);
+      printf("USED ROOT MEM PTR : %p\n", used_root->mem_ptr);
+      printf("START : %p\n", start);
+      printf("P : %p\n", p);
+      printf("FOUND\n");
+      int *a = (int *)p;
+      printf("Value : %d\n", *a);
+      break;
+    }
+    start = start + __alignof__(uintptr_t *);
+  }
+  return;
 }
 
 void __internal_print() {
